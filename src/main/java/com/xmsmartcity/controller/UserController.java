71529f2a142a88886cdcce109895996d752ab3f9
EzerXie <597773658@qq.com>
2018-01-19 11:55:07 +0800

    xzx:老朱是猪

--- src/main/java/com/xmsmartcity/controller/UserController.java
+++ src/main/java/com/xmsmartcity/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UserController {
 
-    //我也参与了这个项目
+    //我也参与了这个项目   啦啦啦
     @RequestMapping(value="/user",method = RequestMethod.GET)
     @ResponseBody
     private String user(){
