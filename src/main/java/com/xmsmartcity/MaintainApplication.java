095bc336836b75bed70c3dc39fd97c09059e3e90
大無 <aizgw2012@163.com>
2018-01-19 10:30:11 +0800

    init

---
+++ src/main/java/com/xmsmartcity/MaintainApplication.java
@@ -0,0 +1,12 @@
+package com.xmsmartcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintainApplication.class, args);
+	}
+}
---
+++ src/main/java/com/xmsmartcity/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.xmsmartcity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Owner on 2018/1/19 0019.
+ */
+@Controller
+public class UserController {
+
+    @RequestMapping(value="/user",method = RequestMethod.GET)
+    @ResponseBody
+    private String user(){
+        return "json";
+    }
+}
