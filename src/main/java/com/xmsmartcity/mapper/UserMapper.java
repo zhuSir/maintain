e76f50e5530b830d8bf8dcc8611e869e5ccd0221
大無 <aizgw2012@163.com>
2018-01-19 17:01:46 +0800

    初始化

---
+++ src/main/java/com/xmsmartcity/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package main.java.com.xmsmartcity.mapper;
+
+import main.java.com.xmsmartcity.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ src/main/java/com/xmsmartcity/pojo/User.java
@@ -0,0 +1,33 @@
+package main.java.com.xmsmartcity.pojo;
+
+public class User {
+    private String id;
+
+    private String userId;
+
+    private String orgId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+
+    public String getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(String orgId) {
+        this.orgId = orgId == null ? null : orgId.trim();
+    }
+}
