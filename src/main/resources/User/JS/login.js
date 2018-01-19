0311fba21ef596408247a2c8acee89ffc2b8c0c4
ydZhang <1693885383@qq.com>
2018-01-19 15:14:28 +0800

    增加 bootStrap文件，和 用户模块

---
+++ src/main/resources/User/JS/login.js
@@ -0,0 +1,12 @@
+
+//
+//
+//ReactDOM.render()
+//{
+//    <div class="row">
+//        <div className="col-sm-12 bg">
+//        </div>
+//    </div>
+//    document.getElementById('login');
+//
+//}
