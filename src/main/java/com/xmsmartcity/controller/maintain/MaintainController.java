b77fccd437a9a0d40f5250d2fea83524c1ee6ea8
大無 <aizgw2012@163.com>
2018-01-22 09:13:54 +0800

    添加Maintain模块

---
+++ src/main/java/com/xmsmartcity/controller/maintain/MaintainController.java
@@ -0,0 +1,11 @@
+package com.xmsmartcity.controller.maintain;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Owner on 2018/1/22 0022.
+ */
+@Controller
+public class MaintainController {
+
+}
