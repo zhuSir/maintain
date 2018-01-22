7927bb4c5afead3fa5317892bf0020c3a8dd6c12
大無 <aizgw2012@163.com>
2018-01-22 09:59:23 +0800

    添加项目模块文件，添加公司模块文件

---
+++ src/com/xmsmartcity/pojo/TpProject.java
@@ -0,0 +1,245 @@
+package com.xmsmartcity.pojo;
+
+import java.util.Date;
+
+public class TpProject {
+    private Integer id;
+
+    private String name;
+
+    private Integer depId;
+
+    private Integer bidState;
+
+    private Float lat;
+
+    private Float lng;
+
+    private String provice;
+
+    private String city;
+
+    private String area;
+
+    private String addr;
+
+    private Integer owenerUnitId;
+
+    private Integer constructUnitId;
+
+    private String mark;
+
+    private Date planStartDate;
+
+    private Date planEndDate;
+
+    private String caption;
+
+    private Integer managerId;
+
+    private Integer projectState;
+
+    private Integer auditState;
+
+    private Integer auditPersonId;
+
+    private Date auditDate;
+
+    private Integer createPersonId;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getDepId() {
+        return depId;
+    }
+
+    public void setDepId(Integer depId) {
+        this.depId = depId;
+    }
+
+    public Integer getBidState() {
+        return bidState;
+    }
+
+    public void setBidState(Integer bidState) {
+        this.bidState = bidState;
+    }
+
+    public Float getLat() {
+        return lat;
+    }
+
+    public void setLat(Float lat) {
+        this.lat = lat;
+    }
+
+    public Float getLng() {
+        return lng;
+    }
+
+    public void setLng(Float lng) {
+        this.lng = lng;
+    }
+
+    public String getProvice() {
+        return provice;
+    }
+
+    public void setProvice(String provice) {
+        this.provice = provice == null ? null : provice.trim();
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city == null ? null : city.trim();
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area == null ? null : area.trim();
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr == null ? null : addr.trim();
+    }
+
+    public Integer getOwenerUnitId() {
+        return owenerUnitId;
+    }
+
+    public void setOwenerUnitId(Integer owenerUnitId) {
+        this.owenerUnitId = owenerUnitId;
+    }
+
+    public Integer getConstructUnitId() {
+        return constructUnitId;
+    }
+
+    public void setConstructUnitId(Integer constructUnitId) {
+        this.constructUnitId = constructUnitId;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark == null ? null : mark.trim();
+    }
+
+    public Date getPlanStartDate() {
+        return planStartDate;
+    }
+
+    public void setPlanStartDate(Date planStartDate) {
+        this.planStartDate = planStartDate;
+    }
+
+    public Date getPlanEndDate() {
+        return planEndDate;
+    }
+
+    public void setPlanEndDate(Date planEndDate) {
+        this.planEndDate = planEndDate;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption == null ? null : caption.trim();
+    }
+
+    public Integer getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Integer managerId) {
+        this.managerId = managerId;
+    }
+
+    public Integer getProjectState() {
+        return projectState;
+    }
+
+    public void setProjectState(Integer projectState) {
+        this.projectState = projectState;
+    }
+
+    public Integer getAuditState() {
+        return auditState;
+    }
+
+    public void setAuditState(Integer auditState) {
+        this.auditState = auditState;
+    }
+
+    public Integer getAuditPersonId() {
+        return auditPersonId;
+    }
+
+    public void setAuditPersonId(Integer auditPersonId) {
+        this.auditPersonId = auditPersonId;
+    }
+
+    public Date getAuditDate() {
+        return auditDate;
+    }
+
+    public void setAuditDate(Date auditDate) {
+        this.auditDate = auditDate;
+    }
+
+    public Integer getCreatePersonId() {
+        return createPersonId;
+    }
+
+    public void setCreatePersonId(Integer createPersonId) {
+        this.createPersonId = createPersonId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
