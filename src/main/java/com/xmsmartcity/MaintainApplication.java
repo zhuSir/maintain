1393c47e3d90464eb68f78f009bffee048776536
大無 <aizgw2012@163.com>
2018-01-22 14:33:29 +0800

    修改数据库驱动及实体类映射xml文件路径

--- src/main/java/com/xmsmartcity/MaintainApplication.java
+++ src/main/java/com/xmsmartcity/MaintainApplication.java
@@ -1,9 +1,11 @@
 package com.xmsmartcity;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.xmsmartcity.mapper")
 public class MaintainApplication {
 
 	public static void main(String[] args) {
