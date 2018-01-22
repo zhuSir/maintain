06b0863e43e531e1236c96042f64ae9dd7236388
大無 <aizgw2012@163.com>
2018-01-22 14:38:08 +0800

    Maintain模块 查询列表接口

--- src/main/java/com/xmsmartcity/controller/maintain/MaintainController.java
+++ src/main/java/com/xmsmartcity/controller/maintain/MaintainController.java
@@ -1,9 +1,13 @@
 package com.xmsmartcity.controller.maintain;
 
+import com.xmsmartcity.pojo.TsMaintain;
 import com.xmsmartcity.service.MaintainService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by zhugw on 2018/1/22 0022.
@@ -16,9 +20,8 @@ public class MaintainController {
     private MaintainService service;
 
     @RequestMapping(value = "/list")
-    public String maintainList(){
-        service.selectList();
-
-        return null;
+    public List<TsMaintain> maintainList(){
+        List<TsMaintain> results = service.selectList();
+        return results;
     }
 }
--- src/main/java/com/xmsmartcity/service/MaintainService.java
+++ src/main/java/com/xmsmartcity/service/MaintainService.java
@@ -1,18 +1,13 @@
 package com.xmsmartcity.service;
 
-import com.xmsmartcity.mapper.TsMaintainMapper;
 import com.xmsmartcity.pojo.TsMaintain;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by Owner on 2018/1/22 0022.
  */
-@Service
 public interface MaintainService extends BaseService<TsMaintain>{
 
     public List<TsMaintain> selectList();
-
 }
