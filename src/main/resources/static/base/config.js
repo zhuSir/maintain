6cd0bff7bf3614206b9f25203ad3b11723fdfe05
hantailala <hantailala@gmail.com>
2018-01-24 09:10:54 +0800

    创建接口配置文件

---
+++ src/main/resources/static/base/config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by zhangzhifu on 2018/1/24.
+ */
+
+var network = "http://";
+
+var host = "";
+
+var config = {
+
+    host: host,
+
+    register: network + host + ""
+
+
+};
--- src/main/resources/static/user/js/register.js
+++ src/main/resources/static/user/js/register.js
@@ -1,12 +1,28 @@
-$(function(){
-   $("#register").click(function() {
+$(function () {
 
-      window.open("creatCompany.html");
+    $("#register").click(function () {
 
+        // window.open("creatCompany.html");
 
-   });
+        var  data ={
+            phone:$("#phone").val(),
+            userName:$("#name").val(),
+            password:$("#psw").val()
+        };
 
-
+        $.ajax({
+            type: "POST",
+            url: zydUrl.userlogin,
+            data: data,
+            dataType: "json",
+            success: function (data) {
+                alert("success");
+            },
+            error: function (err) {
+                alert("error");
+            }
+        });
+    });
 
 
 });
