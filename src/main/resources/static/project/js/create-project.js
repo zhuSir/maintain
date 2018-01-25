2ed86aa9f84737d9cc6dcc9fe3b3131b222e9731
王华 <761406501@qq.com>
2018-01-25 11:01:10 +0800

    项目列表（创建项目弹框），项目详情（编辑项目，关联项目弹框）

--- src/main/resources/static/project/js/create-project.js
+++ src/main/resources/static/project/js/create-project.js
@@ -1,4 +1,4 @@
-var Projects=React.createClass({
+/*var Projects=React.createClass({
     getInitialState: function() {
         return {
             Projects : []
@@ -8,7 +8,7 @@ var Projects=React.createClass({
     componentDidMount: function() {
         this.serverRequest = $.post("localhost:8080/project/select","uId:1",  function (result) {
             this.setState({
-                Projects : result.data
+                Projects : result
             });
 
 
@@ -20,40 +20,86 @@ var Projects=React.createClass({
     },
 
     render:function () {
-        var datas=this.state.Projects.map(function(item){
+        var datas=this.state.Projects.map(function(){
             return (
-
+                Projects : {}
             )
     }
+})*/
+/*编辑项目*/
+var Mask=React.createClass({
+    render:function(){
+        return(
+            <div>
+                <button className=" btn btn-default col-sm-12 top25 text-center test edit" data-toggle="modal" data-target="#myModal" >编辑项目</button>
+                <div className="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                <h4 className="modal-title" id="myModalLabel">编辑项目</h4>
+                            </div>
+                            <div className="modal-body">
+                                <form action="" className="bs-example bs-example-form">
+                                    <input type="text" className="form-control" id="pname" placeholder="项目名称"/><br/>
+                                    <input type="text" className="form-control" id="coname" placeholder="公司名称"/><br/>
+                                    <input type="text" className="form-control" id="fzr" placeholder="项目负责人"/><br/>
+                                    <input type="text" className="form-control" id="cbuid" placeholder="建设单位"/><br/>
+                                    <input type="text" className="form-control" id="department" placeholder="归属公司"/><br/>
+                                    <input type="text" className="form-control" id="discribe" placeholder="备注"/><br/>
+                                    <input type="text" className="form-control" id="begindate" placeholder="计划开始时间"/><br/>
+                                    <input type="text" className="form-control" id="endate" placeholder="计划结束时间"/><br/>
+                                    <input type="text" className="form-control" id="tobia" placeholder="投标状态"/><br/>
+                                    <input type="text" className="form-control" id="prstate" placeholder="项目状态"/><br/>
+                                </form>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-default" data-dismiss="modal">关闭</button>
+                                <button type="button" className="btn btn-primary">提交更改</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 })
-var Project = React.createClass({
+
+/*关联项目*/
+var Reinf=React.createClass({
+    render:function () {
+       return(
+           <div>
+               <div className="btn-group guanli">
+                   <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" >
+                       关联项目 <span class="caret"></span>
+                   </button>
+                   <ul className="dropdown-menu" role="menu">
+                       <li><a href="#"></a></li>
+                       <li><a href="#">Another action</a></li>
+                       <li><a href="#">Something else here</a></li>
+                       <li className="divider"></li>
+                       <li><a href="#">Separated link</a></li>
+                   </ul>
+               </div>
+           </div>
+       )
+   }
+})
+var ContentUI = React.createClass({
     getInitialState: function() {
         return {
-            Projects : []
+            Projects : {}
         };
-
     },
     componentDidMount: function() {
         this.serverRequest = $.post("localhost:8080/project/select","uId:1",  function (result) {
             this.setState({
-                Projects : result.data
+                Projects : result
             });
-
 
         }.bind(this));
     },
-
-    render:function(){
-        var list=this.state.groupList.map(function(item){
-            return (
-                <li className="list-group-item">
-                <p className="navbar-text">项目编号: {item.id} <span className="numberl" id="numberl">03</span> </p>
-             </li>
-            )
-        })
-
-
-
 
 
     render: function() {
@@ -62,15 +108,14 @@ var Project = React.createClass({
             <h3>项目详情</h3>
             <div className="wrap">
                 <ul className="list-group">
-                    {list}
                     <li className="list-group-item">
-                        <p className="navbar-text">项目名称:  <span className="proname" id="proname">维修管理</span> </p>
-                </li>
-                    <li className="list-group-item">
-                        <p className="navbar-text">公司名称:  <span className="comname" id="comname">小康建材</span> </p>
+                        <p className="navbar-text">项目名称:  <span className="proname" id="proname">3</span> </p>
                     </li>
                     <li className="list-group-item">
-                        <p className="navbar-text">项目负责人：<span className="forperson" id="forperson">张三</span></p>
+                        <p className="navbar-text">公司名称:  <span className="comname" id="comname">大山</span> </p>
+                    </li>
+                    <li className="list-group-item">
+                        <p className="navbar-text">项目负责人：<span className="forperson" id="forperson">咋和你说噶</span></p>
                     </li>
                     <li className="list-group-item">
                         <p className="navbar-text">建设单位： <span className="build" id="build">厦门承建有限公司</span></p>
@@ -78,9 +123,9 @@ var Project = React.createClass({
                     <li className="list-group-item">
                         <p className="navbar-text">归属部门：<span className="deparment" id="deparment">研发部</span></p>
                     </li>
-                    <li className="list-group-item">
+                    <li className="list-group-item beish">
                         <div className="reference">
-                            <p className="navbar-text">备注: <div className="discribe">我的描述详情</div></p>
+                            <p className="navbar-text">备注: <div className="discribe" id="discribe">我的描述详情</div></p>
                         </div>
                     </li>
                     <li className="list-group-item">
@@ -97,21 +142,8 @@ var Project = React.createClass({
             <div className="prostatus">
                 <p className="navbar-text">项目状态：<span className="">未开工</span></p>
             </div>
-
-
-            <div className="btn-group">
-                <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" onclick={this.pulling}>
-                    Action <span class="caret"></span>
-                </button>
-                <ul className="dropdown-menu" role="menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li className="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                </ul>
-            </div>
-
+            <Reinf/>
+            <Mask/>
             <div className="admin">
                 <p className="navbar-text">审核人：<span className="auditor">张朝阳</span></p>
                 <p className="navbar-text">审核日期：<span className="auditor-date">2017-12-29</span></p>
@@ -122,7 +154,7 @@ var Project = React.createClass({
     }
 });
 ReactDOM.render(
-    <Project />,
+    <ContentUI />,
     document.getElementById('example')
 );
 /*
