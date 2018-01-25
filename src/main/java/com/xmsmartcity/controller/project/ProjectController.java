23e3cd1d1fca6a2ebcaaf8c68f0a9abec3093456
EzerXie <597773658@qq.com>
2018-01-25 10:06:30 +0800

    xzx:项目修改

--- src/main/java/com/xmsmartcity/controller/project/ProjectController.java
+++ src/main/java/com/xmsmartcity/controller/project/ProjectController.java
@@ -84,6 +84,55 @@ public class ProjectController {
         return null;
     }
 
+    @RequestMapping(value = "/update")
+    public String updateProject(TpProject project,@Param("startData") String startData, @Param("endDate") String endDate){
+        try {
+            if(project.getId()<=0){
+                return "参数错误,修改失败";
+            }
+            if(project.getName()==null||"".equals(project.getName())){
+                return "参数错误,修改失败";
+            }
+            if(project.getDepId()==null||project.getDepId().equals("")){
+                return "参数错误,修改失败";
+            }
+            if(project.getOwenerUnitId()==null||project.getOwenerUnitId().equals("")){
+                return "参数错误,修改失败";
+            }
+            if(project.getConstructUnitId()==null||project.getConstructUnitId().equals("")){
+                return "参数错误,修改失败";
+            }
+            if(startData==null||startData.equals("")){
+                return "参数错误,修改失败";
+            }
+            if(endDate==null||endDate.equals("")){
+                return "参数错误,修改失败";
+            }
+            if(project.getManagerId()==null||project.getManagerId().equals("")){
+                return "参数错误,修改失败";
+            }
+            if(project.getAuditPersonId()==null||project.getAuditPersonId().equals("")){
+                return "参数错误,修改失败";
+            }
+            Date date = new Date();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            Timestamp timestamp = new Timestamp(date.getTime());
+            project.setCreateTime(timestamp);
+            project.setUpdateTime(timestamp);
+            timestamp.setTime(sdf.parse(startData).getTime());
+            project.setPlanStartDate(timestamp);
+            timestamp.setTime(sdf.parse(endDate).getTime());
+            project.setPlanEndDate(timestamp);
+            String s = projectService.updateProject(project);
+            if("-1".equals(s)){
+                return "修改失败";
+            }
+            return "修改成功";
+        }catch (Exception e){
+            return "修改失败";
+        }
+    }
+
     @RequestMapping(value = "/select")
     public List<TpProject> selectProject(@NotNull String uId) {
         if (uId != null) {
