d3d5b60f4d414e248f40b34c059b82a3e2b6993b
hantailala <hantailala@gmail.com>
2018-01-26 11:56:15 +0800

    TsFunctionAuthorityMapper

---
+++ src/main/java/com/xmsmartcity/pojo/TsFunctionAuthority.java
@@ -0,0 +1,73 @@
+package com.xmsmartcity.pojo;
+
+public class TsFunctionAuthority {
+    private Integer id;
+
+    private Integer companyid;
+
+    private Integer groupid;
+
+    private Integer fault;
+
+    private Integer update;
+
+    private Integer look;
+
+    private Integer unlookfault;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCompanyid() {
+        return companyid;
+    }
+
+    public void setCompanyid(Integer companyid) {
+        this.companyid = companyid;
+    }
+
+    public Integer getGroupid() {
+        return groupid;
+    }
+
+    public void setGroupid(Integer groupid) {
+        this.groupid = groupid;
+    }
+
+    public Integer getFault() {
+        return fault;
+    }
+
+    public void setFault(Integer fault) {
+        this.fault = fault;
+    }
+
+    public Integer getUpdate() {
+        return update;
+    }
+
+    public void setUpdate(Integer update) {
+        this.update = update;
+    }
+
+    public Integer getLook() {
+        return look;
+    }
+
+    public void setLook(Integer look) {
+        this.look = look;
+    }
+
+    public Integer getUnlookfault() {
+        return unlookfault;
+    }
+
+    public void setUnlookfault(Integer unlookfault) {
+        this.unlookfault = unlookfault;
+    }
+}
