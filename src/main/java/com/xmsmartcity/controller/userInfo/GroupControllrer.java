291ff19a0e328ee78484e198c1cf8846b457ab9f
hantailala <hantailala@gmail.com>
2018-02-03 12:21:06 +0800

    组添加，删除接口

--- src/main/java/com/xmsmartcity/controller/userInfo/GroupControllrer.java
+++ src/main/java/com/xmsmartcity/controller/userInfo/GroupControllrer.java
@@ -1,15 +1,23 @@
 package com.xmsmartcity.controller.userInfo;
 
 
-
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.xmsmartcity.pojo.CommonObjParam;
+import com.xmsmartcity.pojo.CommonObjReturn;
 import com.xmsmartcity.pojo.TsFunctionGroup;
+import com.xmsmartcity.pojo.TsUser;
 import com.xmsmartcity.service.UserGroupService;
 import com.xmsmartcity.service.UserService;
+import com.xmsmartcity.util.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -20,75 +28,104 @@ import java.util.List;
 public class GroupControllrer {
 
     @Autowired
-    private   UserGroupService ser;
+    private UserGroupService ser;
 
     @Autowired
     private UserService userService;
 
     /**
      * 创建公司
+     *
      * @return
      */
-    @RequestMapping(value="/user/creatcompany",method = RequestMethod.POST)
-    private String creatGroup(String companyName,String reatUserName,int reatUserID){
-
-        String resault =  ser.creatGroup(companyName,reatUserName,reatUserID);
-
+    @RequestMapping(value = "/user/creatcompany", method = RequestMethod.POST)
+    private String creatGroup(String companyName, String reatUserName, int reatUserID) {
+        String resault = ser.creatGroup(companyName, reatUserName, reatUserID);
         return resault;
     }
 
     /**
      * 创建组
+     *
      * @return
      */
-    @RequestMapping(value="/user/creatGroup",method = RequestMethod.POST)
-    private String creatGroup(String groupname,String companyID,String reatUserName,int reatUserID){
-
-         String resault =  ser.insertGroup(groupname,companyID,reatUserName,reatUserID);
-
-        return resault;
+    public CommonObjReturn creatGroup(@RequestBody CommonObjParam objparam, HttpServletRequest request, HttpServletResponse response) {
+        TsFunctionGroup resultData = null;
+        HashMap map = (HashMap) objparam.getData();
+        String groupname = map.get("groupname").toString();
+        String companyID = map.get("companyID").toString();
+        String reatUserName = map.get("reatUserName").toString();
+        Integer reatUserID = Integer.valueOf(map.get("reatUserID").toString());
+        String result = ser.insertGroup(groupname, companyID, reatUserName, reatUserID);
+        if ("1".equals(result)){
+            resultData = ser.getGroup(companyID,groupname);
+        }
+        CommonObjReturn commonObjReturn = new CommonObjReturn();
+        commonObjReturn.setData(resultData);
+        commonObjReturn.setResult("true");
+        commonObjReturn.setResultTime(DateUtils.DateToString(new Date(), DateUtils.formatStr_yyyyMMddHHmmss));
+        return commonObjReturn;
     }
 
-
     //获取子组的列表
-    @RequestMapping(value="/user/getGroupList",method = RequestMethod.POST)
-    private List getGroupList(String sonGroupID){
+    public CommonObjReturn getGroupList(@RequestBody CommonObjParam objparam, HttpServletRequest request, HttpServletResponse response) {
+        HashMap map = (HashMap) objparam.getData();
+        String sonGroupID = map.get("companyId").toString();
+        List<TsFunctionGroup> result = ser.getGroupList(sonGroupID);
+        CommonObjReturn commonObjReturn = new CommonObjReturn();
+        commonObjReturn.setData(result);
+        commonObjReturn.setResult("true");
+        commonObjReturn.setResultTime(DateUtils.DateToString(new Date(), DateUtils.formatStr_yyyyMMddHHmmss));
+        return commonObjReturn;
+    }
 
-        List<TsFunctionGroup> result  = ser.getGroupList(sonGroupID);
-        return result;
+    //删除子组
+    public CommonObjReturn removeGroup(@RequestBody CommonObjParam objparam, HttpServletRequest request, HttpServletResponse response) {
+        HashMap map = (HashMap) objparam.getData();
+        String sonGroupID = map.get("id").toString();
+        JSONObject result = ser.removeGroup(sonGroupID);
+        CommonObjReturn commonObjReturn = new CommonObjReturn();
+        commonObjReturn.setData(result);
+        commonObjReturn.setResult("true");
+        commonObjReturn.setResultTime(DateUtils.DateToString(new Date(), DateUtils.formatStr_yyyyMMddHHmmss));
+        return commonObjReturn;
     }
 
     //获取公司的详情
-    @RequestMapping(value="/user/getCompanyInfo",method = RequestMethod.POST)
-    private TsFunctionGroup getcompanyInfo(int companyID){
-        TsFunctionGroup result  = ser.getcompanyInfo(companyID);
+    @RequestMapping(value = "/user/getCompanyInfo", method = RequestMethod.POST)
+    private TsFunctionGroup getcompanyInfo(int companyID) {
+        TsFunctionGroup result = ser.getcompanyInfo(companyID);
         return result;
     }
 
     @GetMapping(value = "/user/companyInviteMember")
-    private String companyInviteMenber(String phone,String companyID)
-    {
-        String  result = ser.companyInviteMenber(phone,companyID);
+    private String companyInviteMenber(String phone, String companyID) {
+        String result = ser.companyInviteMenber(phone, companyID);
 
         return result;
     }
 
     /**
      * 获取公司成员
-     * @param companyID
+     *
+     * @param
      * @return
      */
-    @RequestMapping(value="/user/listCompanyMember",method = RequestMethod.POST)
-    private JSONObject listCompanyMember(int companyID){
-        JSONObject result = userService.selectUserList(companyID);
-        return result;
+    public CommonObjReturn listCompanyMember(@RequestBody CommonObjParam objparam, HttpServletRequest request, HttpServletResponse response) {
+        TsUser tsUser = JSON.parseObject(JSON.toJSONString(objparam.getData()), TsUser.class);
+        JSONObject result = userService.selectUserList(tsUser.getCompanyid());
+        CommonObjReturn commonObjReturn = new CommonObjReturn();
+        commonObjReturn.setData(result);
+        commonObjReturn.setResult("true");
+        commonObjReturn.setResultTime(DateUtils.DateToString(new Date(), DateUtils.formatStr_yyyyMMddHHmmss));
+        return commonObjReturn;
     }
 
     //邀请组成员
-    @RequestMapping(value = "/user/invitePeopleGroup",method = RequestMethod.POST)
-    public Object invitePeopleGroup(String phone,String companyID,String groupID){
+    @RequestMapping(value = "/user/invitePeopleGroup", method = RequestMethod.POST)
+    public Object invitePeopleGroup(String phone, String companyID, String groupID) {
 
-        Object  result = userService.invitePeopleGroup(phone,companyID,groupID);
+        Object result = userService.invitePeopleGroup(phone, companyID, groupID);
         return result;
     }
 
--- src/main/java/com/xmsmartcity/controller/userInfo/UserController.java
+++ src/main/java/com/xmsmartcity/controller/userInfo/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -30,70 +31,78 @@ public class UserController {
 
     /**
      * 注册接口
+     *
      * @return
      */
-    public CommonObjReturn register(@RequestBody CommonObjParam objparam, HttpServletRequest request, HttpServletResponse response){
-        TsUser tsUser=JSON.parseObject(JSON.toJSONString(objparam.getData()),TsUser.class);
+    public CommonObjReturn register(@RequestBody CommonObjParam objparam, HttpServletRequest request, HttpServletResponse response) {
+        TsUser tsUser = JSON.parseObject(JSON.toJSONString(objparam.getData()), TsUser.class);
         String result = userService.addUser(tsUser);
-        CommonObjReturn commonObjReturn=new CommonObjReturn();
+        CommonObjReturn commonObjReturn = new CommonObjReturn();
         commonObjReturn.setData(result);
         commonObjReturn.setResult("true");
-        commonObjReturn.setResultTime(DateUtils.DateToString(new Date(),DateUtils.formatStr_yyyyMMddHHmmss));
+        commonObjReturn.setResultTime(DateUtils.DateToString(new Date(), DateUtils.formatStr_yyyyMMddHHmmss));
         return commonObjReturn;
     }
 
     /**
      * 登陆接口
+     *
      * @return
      */
-    public CommonObjReturn login(@RequestBody CommonObjParam objparam, HttpServletRequest request, HttpServletResponse response){
-        TsUser tsUser=JSON.parseObject(JSON.toJSONString(objparam.getData()),TsUser.class);
+    public CommonObjReturn login(@RequestBody CommonObjParam objparam, HttpServletRequest request, HttpServletResponse response) {
+        TsUser tsUser = JSON.parseObject(JSON.toJSONString(objparam.getData()), TsUser.class);
         String result = userService.selectUser(tsUser);
-        CommonObjReturn commonObjReturn=new CommonObjReturn();
+        CommonObjReturn commonObjReturn = new CommonObjReturn();
         commonObjReturn.setData(result);
         commonObjReturn.setResult("true");
-        commonObjReturn.setResultTime(DateUtils.DateToString(new Date(),DateUtils.formatStr_yyyyMMddHHmmss));
+        commonObjReturn.setResultTime(DateUtils.DateToString(new Date(), DateUtils.formatStr_yyyyMMddHHmmss));
         return commonObjReturn;
     }
 
     /**
      * id获取用户接口
+     *
      * @return
      */
-    @RequestMapping(value="/getUserById",method = RequestMethod.POST)
-    private TsUser getUserById(String userId){
+    @RequestMapping(value = "/getUserById", method = RequestMethod.POST)
+    private TsUser getUserById(String userId) {
         Integer id = Integer.valueOf(userId);
         TsUser result = userService.selectUserById(id);
         return result;
     }
 
     @GetMapping("/list")
-    public List selectList(String phone,String name){
-        List<Map<String,Object>> res = userService.selectList(phone,name);
+    public List selectList(String phone, String name) {
+        List<Map<String, Object>> res = userService.selectList(phone, name);
         return res;
     }
 
     /**
      * 用户手机查询名称接口
+     *
      * @return
      */
-    @RequestMapping(value="/byphone")
-    public TsUser selectById(String phone){
+    @RequestMapping(value = "/byphone")
+    public TsUser selectById(String phone) {
         TsUser result = userService.selectByPhone(phone);
         return result;
     }
 
     /**
      * id获取移除公司
+     *
      * @return
      */
-    @RequestMapping(value="/deleteGroupUser",method = RequestMethod.POST)
-    private JSONObject deleteGroupUser(String userId){
-        Integer id = Integer.valueOf(userId);
+    public CommonObjReturn deleteGroupUser(@RequestBody CommonObjParam objparam, HttpServletRequest request, HttpServletResponse response) {
+        HashMap map = (HashMap) objparam.getData();
+        Integer id = Integer.valueOf(map.get("id").toString());
         JSONObject result = userService.deleteGroupUser(id);
-        return result;
+        CommonObjReturn commonObjReturn = new CommonObjReturn();
+        commonObjReturn.setData(result);
+        commonObjReturn.setResult("true");
+        commonObjReturn.setResultTime(DateUtils.DateToString(new Date(), DateUtils.formatStr_yyyyMMddHHmmss));
+        return commonObjReturn;
     }
-
 
 
 }
