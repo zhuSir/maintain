0a756fe432aa170ad44bf260ee6a93ae6a8680a1
ydZhang <1693885383@qq.com>
2018-02-06 17:22:28 +0800

    测试合并

--- src/main/java/com/xmsmartcity/mapper/TsUserMapper.java
+++ src/main/java/com/xmsmartcity/mapper/TsUserMapper.java
@@ -20,7 +20,7 @@ public interface TsUserMapper extends BaseDao<TsUser> {
     List<TsUser> listGroupMember(String groupId);
 
     //邀请用户到组
-    int invitePeopleGroup(@Param("phone")String phone,@Param("groupID")String groupID,@Param("companyID")String companyID);
+    int invitePeopleGroup(@Param("phone")String phone,@Param("companyID")String companyID,@Param("groupID")String groupID);
 
     //更新用户的公司ID
     int updateUserCompanyInfo(@Param("userId")int userId,@Param("companyID")int companyID,@Param("companyName")String companyName);
